88d2cd3f99e2c5ca59ba9d88428b4dff039970cc
Regis Tangout <rtangou@gmail.com>
2024-09-11 22:46:24 +0100

    indexhtml and Appjs

--- backend/spring-boot-library/frontend/src/App.js
+++ backend/spring-boot-library/frontend/src/App.js
@@ -1,7 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from '.pages/Home';
+import Login from '.pages/Login';
+import MainPge from '.pages/MainPage';
+import Register from '.pages/Register';
+import AboutAuthor from '.pages/AboutAuthor';
+import Overview from '.pages/Overview';
+import ForgotPassword from '.pages/ForgotPassword';
+import ForgotUsername from '.pages/ForgotUsername';
+import Message from '.pages/Message';
+import Setting from '.pages/Setting';
 
 function App() {
+  return(
+     <Router>
+     <Switch>
+     <Route Path="/login" component={Login} />
+     <Route Path="/forgot-password" component={ForgotPassword} />
+     <Route Path="/register" component={Register} />
+     <Route Path="/main-page" component={MainPage} />
+     <Route Path="/overview" component={Overview} />
+
+     </Switch>
+    </Router>
+
+  );
+}
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -13,13 +41,13 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="opener referrer"
         >
           Learn React
         </a>
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
