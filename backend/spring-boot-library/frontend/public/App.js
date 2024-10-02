062f95c78ccf449e948286106ebead28cd417d0d
Regis Tangout <rtangou@gmail.com>
2024-10-02 19:32:51 +0100

    petite modif

--- backend/spring-boot-library/frontend/public/App.js
+++ backend/spring-boot-library/frontend/public/App.js
@@ -15,6 +15,7 @@ function App() {
   return (
     <Router>
       <Routes>
+       <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/register" element={<Register />} />
